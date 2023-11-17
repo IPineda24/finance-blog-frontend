9e0f6cb4e8c0997506d57f143f545474c3a627e8
Irvin Pineda <irvin.pineda024@gmail.com>
2023-11-17 10:51:27 -0600

    :link: Connect the registration part (not validated)

--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -1,15 +1,67 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react';
+import register from '@/app/api/auth/register';
 
-export default function page() {
+export default function Page() {
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password: ''
+  });
+
+
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // Handle form submission here, you can send the data to your server or perform other actions
+    console.log(formData);
+    await register(formData);
+  };
+
   return (
     <div>
       <h1>Register</h1>
-      <form action="" method="post">
-        <input type="text" name="username" id="username" placeholder="Username" />
-        <input type="text" name="email" id="email" placeholder="Email" />
-        <input type="password" name="password" id="password" placeholder="Password" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          placeholder="Username"
+          value={formData.username}
+          onChange={handleChange}
+        />
 
+        <label htmlFor="email">Email:</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          placeholder="Email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <button type="submit">Register</button>
       </form>
     </div>
-  )
+  );
 }
