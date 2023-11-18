7b74f15dd311001e75c7e8b2f452231d89277f48
Irvin Pineda <00024420@uca.edu.sv>
2023-11-18 16:46:58 -0600

    :lock: Validate authentication

--- src/app/api/auth/login/index.ts
+++ src/app/api/auth/login/index.ts
@@ -3,20 +3,23 @@
 import axios from "axios";
 import baseUrl from "@/app/utils/baseUrl";
 import { cookies } from 'next/headers'
+import Swal from "sweetalert2";
 
 const login = async (data:any) => {
     try {
         const response = await axios.post( `${baseUrl}/auth/log-in`, data );
-
-
         console.log( "se inicio sesion" );
         console.log(response.data);
         console.log(response.data.access_token);
         cookies().set( 'token', response.data.access_token );
 
-        
-    } catch (error) {
-    console.log(error);
+    } catch ( error ) {
+            Swal.fire({
+            icon: "error",
+            title: "Ups...",
+            text: "El usuario o la contraseña son incorrectos",
+            footer: `${error}`,
+        });
     }
 };
 export default login;
--- src/app/api/auth/register/index.ts
+++ src/app/api/auth/register/index.ts
@@ -1,17 +1,28 @@
 
 import axios from "axios";
 import baseUrl from "@/app/utils/baseUrl";
+import Swal from "sweetalert2";
+import router from "next/router";
 
-const register = async ( data: any ) => {
+
+
+const register = async ( data: object ) => {
+
     try {
         const response = await axios.post( `${baseUrl}/auth/register`, data );
         console.log( "Cuenta creada con exito" );
+        console.log( response.data );
+        //fixme: redireccionar a login
         const origin = window.location.origin;
         const loginUrl = `${origin}/auth/login`;
         window.location.href = loginUrl;
-        
-    } catch (error) {
-    console.log(error);
+    } catch ( error ) {
+            Swal.fire({
+            icon: "error",
+            title: "Ups...",
+            text: "El usuario o la contraseña ya existen",
+            footer: `${error}`,
+        });
     }
 }; 
 export default register;
