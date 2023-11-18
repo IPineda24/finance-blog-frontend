d23282834dfa245874a19ba76a8aa40283af3f7d
Irvin Pineda <00024420@uca.edu.sv>
2023-11-18 15:47:09 -0600

    feat::sparkles: Add login module, and validate forms with antd :sparkles:

--- src/app/api/auth/register/index.ts
+++ src/app/api/auth/register/index.ts
@@ -1,12 +1,17 @@
+
 import axios from "axios";
 import baseUrl from "@/app/utils/baseUrl";
 
-const register = async (data:any) => {
+const register = async ( data: any ) => {
     try {
         const response = await axios.post( `${baseUrl}/auth/register`, data );
-        console.log("Cuenta creada con exito");
+        console.log( "Cuenta creada con exito" );
+        const origin = window.location.origin;
+        const loginUrl = `${origin}/auth/login`;
+        window.location.href = loginUrl;
+        
     } catch (error) {
     console.log(error);
     }
-};
+}; 
 export default register;
--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,35 +1,72 @@
-'use client';
+'use client'
+import login from '@/app/api/auth/login';
 import React, { useState } from 'react';
-export default function Page() {
-    const [email, setEmail] = useState( '' );
-    const [password, setPassword] = useState( '' );
-    const handleSubmit = ( e: any ) => {
-        e.preventDefault();
+import { Button, Checkbox, Form, Input } from 'antd';
 
-        console.log( 'Email:', email );
-        console.log( 'Password:', password );
+export default function Page() {
+    const onFinish = async ( values: any ) => {
+        console.log( 'Success:', values );
+        const userData = {
+            email: values.email,
+            password: values.password
+        }
+        await login( userData );
+
     };
+
+    const onFinishFailed = ( errorInfo: any ) => {
+        console.log( 'Failed:', errorInfo );
+    };
+
+    type FieldType = {
+        email?: string;
+        password?: string;
+        remember?: string;
+    };
+
 
     return (
         <div>
-            <h1>Login Page</h1>
-            <p>Create login like register page</p>
-            <form onSubmit={handleSubmit}>
-                {/* Asignar las funciones de cambio de estado a los eventos onChange */}
-                <input
-                    type="email"
-                    placeholder="email"
-                    value={email}
-                    onChange={( e ) => setEmail( e.target.value )}
-                />
-                <input
-                    type="password"
-                    placeholder="password"
-                    value={password}
-                    onChange={( e ) => setPassword( e.target.value )}
-                />
-                <button type="submit">Login</button>
-            </form>
+            <Form
+                name="basic"
+                labelCol={{ span: 8 }}
+                wrapperCol={{ span: 16 }}
+                style={{ maxWidth: 600 }}
+                initialValues={{ remember: true }}
+                onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
+                autoComplete="off"
+            >
+                <Form.Item<FieldType>
+                    label="email"
+                    name="email"
+                    rules={[{ required: true, message: 'Please input your email!' }, { type: 'email' }, { whitespace: false }, { pattern: /^\S+@\S+\.\S+$/, message: 'El email no puede contener espacios en blanco.' }, { max: 50 }]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                    label="Password"
+                    name="password"
+                    rules={[{ required: true, message: 'Please input your password!' }, { min: 6 }, { max: 20 }, { type: 'string' }, { whitespace: false }, { pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,20}$/, message: 'La contraseña debe contener al menos 6 caracteres, incluyendo mayúsculas/minúsculas y números.' }]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item<FieldType>
+                    name="remember"
+                    valuePropName="checked"
+                    wrapperCol={{ offset: 8, span: 16 }}
+                >
+                    <Checkbox>Remember me</Checkbox>
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+                    <Button type="primary" htmlType="submit">
+                        Submit
+                    </Button>
+                </Form.Item>
+            </Form>
         </div>
     );
 }
