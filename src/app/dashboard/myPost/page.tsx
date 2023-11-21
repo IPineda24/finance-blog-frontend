dac6296d7c28659e84a92b5eb241aabea1544cfa
Irvin Pineda <irvin.pineda024@gmail.com>
2023-11-21 10:19:34 -0600

    feat: :pencil: Add post editing functionality
    :art: Improve styling for edit form.

--- src/app/dashboard/myPost/page.tsx
+++ src/app/dashboard/myPost/page.tsx
@@ -4,42 +4,44 @@ import React, { useEffect, useState } from 'react';
 import PostEditList from '@/app/components/postEditList';
 import { getPost } from '@/app/api/post/myPost';
 
+interface Post {
+    id: number;
+    title: string;
+    description: string;
+    likes: number;
+    createdAt: string;
+    createdBy: {
+        id: number;
+        username: string;
+    };
+}
 
 const MyPostPage: React.FC = () => {
-    interface Post {
-        id: number;
-        title: string;
-        description: string;
-        likes: number;
-        createdAt: string;
-        createdBy: {
-            id: number;
-            username: string;
-        };
-    }
-
     const [posts, setPosts] = useState<Post[]>( [] );
 
+    const fetchPosts = async () => {
+        try {
+            const fetchedPosts: Post[] = await getPost();
+            setPosts( fetchedPosts );
+        } catch ( error ) {
+            console.error( 'Error fetching posts:', error );
+            // Handle error, e.g., show an error message
+        }
+    };
+
     useEffect( () => {
-        const fetchPosts = async () => {
-            try {
-                const fetchedPosts: Post[] = await getPost();
-                setPosts( fetchedPosts );
-
-            } catch ( error ) {
-                console.error( 'Error fetching posts:', error );
-                // Handle error, e.g., show an error message
-            }
-        };
-
         fetchPosts();
     }, [] );
+
+    const updatePosts = async () => {
+        await fetchPosts();
+    };
 
     return (
         <div className="bg-gray-800">
             <div className="bg-gray-800">
                 <h1 className="text-2xl font-bold text-white text-center mb-6 h-2 bg-green-800">My Posts</h1>
-                <PostEditList posts={posts} />
+                <PostEditList posts={posts} onPostUpdate={updatePosts} />
             </div>
         </div>
     );
