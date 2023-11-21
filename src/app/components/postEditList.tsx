781fab7873ae3474795a26be6f7aadb3f6789974
Irvin Pineda <irvin.pineda024@gmail.com>
2023-11-21 10:19:34 -0600

    feat: :pencil: Add post editing functionality
    :art: Improve styling for edit form.

--- src/app/components/postEditList.tsx
+++ src/app/components/postEditList.tsx
@@ -1,6 +1,6 @@
 // components/PostEditList.tsx
-import React from 'react';
-import { format } from 'date-fns';
+import React, { useState } from 'react';
+import { Form, Input, Button } from 'antd';
 import { updatePost } from '@/app/api/post/myPost';
 
 interface Post {
@@ -14,32 +14,50 @@ interface Post {
         username: string;
     };
 }
+
 interface PostEditListProps {
     posts: Post[];
+    onPostUpdate: () => Promise<void>;
 }
 
-const PostEditList: React.FC<PostEditListProps> = ( { posts } ) => {
-    const handleEditPost = async ( postId: number ) => {
-        // Implement your logic for editing the post, e.g., opening a modal
-        console.log( "Editing post:", postId );
-        // You can use state to manage the editing form/modal visibility
+const PostEditList: React.FC<PostEditListProps> = ( { posts, onPostUpdate } ) => {
+    const [form] = Form.useForm();
+    const [editingPostId, setEditingPostId] = useState<number | null>( null );
 
-        // Example: Update the post with some data
-        const updatedData = {
-            // Provide the updated data for the post
-        };
+    const handleEditPost = ( postId: number ) => {
+        setEditingPostId( postId );
+        form.setFieldsValue( {
+            title: posts.find( ( post ) => post.id === postId )?.title,
+            description: posts.find( ( post ) => post.id === postId )?.description,
+        } );
+    };
 
+    const handleCancelEdit = () => {
+        setEditingPostId( null );
+        form.resetFields();
+    };
+
+    const handleUpdatePost = async () => {
         try {
-            const success = await updatePost( postId, updatedData );
+            const values = await form.validateFields();
+
+            const valuesFormatted = {
+                title: values.title,
+                description: values.description,
+            };
+
+            console.log( editingPostId!, valuesFormatted );
+            const success = await updatePost( editingPostId!, valuesFormatted );
             if ( success ) {
                 console.log( 'Post updated successfully' );
+                handleCancelEdit();
+                // Llamar a la función de actualización de la lista después de la edición exitosa
+                await onPostUpdate();
             } else {
                 console.error( 'Failed to update post' );
-                // Handle error, e.g., show an error message
             }
         } catch ( error ) {
             console.error( 'Error updating post:', error );
-            // Handle error, e.g., show an error message
         }
     };
 
@@ -47,22 +65,55 @@ const PostEditList: React.FC<PostEditListProps> = ( { posts } ) => {
         <div className="max-w-2xl mx-auto">
             {posts.map( ( post: Post ) => (
                 <div key={post.id} className="border bg-white border-gray-300 mb-4 rounded overflow-hidden">
+                    {/* ... (existing code) */}
                     <div className="p-4">
-                        <div className="flex items-center mb-2">
+                        {editingPostId === post.id ? (
+                            <Form form={form} onFinish={handleUpdatePost}>
+                                <Form.Item
+                                    name="title"
+                                    initialValue={post.title}
+                                    rules={[
+                                        { required: true, message: 'Por favor, ingresa el título' },
+                                        { max: 100, message: 'El título debe tener como máximo 100 caracteres' },
+                                        { min: 5, message: 'El título debe tener al menos 5 caracteres' },
+                                        { whitespace: true, message: 'Por favor, ingresa un título válido' },
+                                        { type: 'string', message: 'Por favor, ingresa un título válido' },
+                                    ]}
+                                >
+                                    <Input placeholder="Title" />
+                                </Form.Item>
+                                <Form.Item
+                                    name="description"
+                                    initialValue={post.description}
+                                    rules={[
+                                        { required: true, message: 'Por favor, ingresa la descripción' },
+                                        { max: 1000, message: 'La descripción debe tener como máximo 1000 caracteres' },
+                                        { min: 10, message: 'La descripción debe tener al menos 10 caracteres' },
+                                        { whitespace: true, message: 'Por favor, ingresa una descripción válida' },
+                                        { type: 'string', message: 'Por favor, ingresa una descripción válida' },
+                                    ]}
+                                >
+                                    <Input.TextArea placeholder="Description" />
+                                </Form.Item>
+                                <Form.Item>
+                                    <Button type="primary" htmlType="submit">
+                                        Update
+                                    </Button>
+                                    <Button onClick={handleCancelEdit} style={{ marginLeft: 8 }}>
+                                        Cancel
+                                    </Button>
+                                </Form.Item>
+                            </Form>
+                        ) : (
                             <div>
-                                <p className="text-sm font-semibold text-gray-600">{post.createdBy.username}</p>
-                                <p className="text-xs text-gray-500">{format( new Date( post.createdAt ), 'dd/MM/yyyy h:mm a' )}</p>
+                                <h2 className="text-lg font-semibold mb-2">{post.title}</h2>
+                                <p className="text-sm mb-4">{post.description}</p>
                             </div>
-                        </div>
-                        <h2 className="text-lg font-semibold mb-2">{post.title}</h2>
-                        <p className="text-sm mb-4">{post.description}</p>
+                        )}
                     </div>
                     <div className="p-4">
                         <div className="flex items-center space-x-2">
-                            <button
-                                className="text-xs text-gray-500 ml-2"
-                                onClick={() => handleEditPost( post.id )}
-                            >
+                            <button className="text-xs text-gray-500 ml-2" onClick={() => handleEditPost( post.id )}>
                                 Edit
                             </button>
                         </div>
