a4cc8472def14033a8cbdde8000145ee9deafb0b
Irvin Pineda <irvin.pineda024@gmail.com>
2023-11-22 11:00:33 -0600

    Feat: :sparkles: Comments post function :sparkles:

--- src/app/components/PostList.tsx
+++ src/app/components/PostList.tsx
@@ -1,21 +1,37 @@
 'use client';
+
 import React, { useState, useEffect } from 'react';
 import handleLikes from '@/app/api/post/like';
 import { format } from 'date-fns';
 import { Input, Button } from 'antd';
-import { Dropdown, Menu } from 'antd';
-import { HeartOutlined } from '@ant-design/icons';
+import { CommentOutlined, HeartOutlined, SendOutlined } from '@ant-design/icons';
 import { Post } from '@/app/interfaces/types';
+import { addComment } from '@/app/api/post/comment';
+import { getPost } from '../api/post/myPost';
+
+const { TextArea } = Input;
+
 interface PostListProps {
     posts: Post[];
 }
-const { TextArea } = Input;
 
 const PostList: React.FC<PostListProps> = ( { posts } ) => {
     const [likes, setLikes] = useState<number[]>( [] );
     const [likedPosts, setLikedPosts] = useState<boolean[]>( [] );
     const [newComment, setNewComment] = useState<string>( '' );
+    const [showComments, setShowComments] = useState<boolean[]>( [] );
     const userId = localStorage.getItem( 'userId' );
+
+    useEffect( () => {
+        setLikes( posts.map( ( post ) => post.likes ) );
+        setLikedPosts(
+            posts.map( ( post ) =>
+                post.receivedLikes.some( ( like ) => like.userId === userId )
+            )
+        );
+        //que se actualice el nuevo comentario 
+        setShowComments( new Array( posts.length ).fill( false ) );
+    }, [posts, newComment] );
 
     const handleLike = async ( index: number, postId: number ) => {
         try {
@@ -33,40 +49,49 @@ const PostList: React.FC<PostListProps> = ( { posts } ) => {
         }
     };
 
-    const handleAddComment = ( index: number, postId: number ) => {
-        // ... (implementa la lógica para agregar comentarios)
+    const handleAddComment = async ( index: number, postId: number ) => {
+        try {
+            if ( newComment.trim() === '' ) {
+                return;
+            }
+
+            const response = await addComment( postId, newComment );
+            const updatedPosts = [...posts];
+            updatedPosts[index].comments.push( response );
+
+            setNewComment( '' );
+
+            window.location.reload();
+        } catch ( error ) {
+            console.error( 'Error al agregar comentario', error );
+        }
     };
-
-
-
-    useEffect( () => {
-        setLikes( posts.map( ( post ) => post.likes ) );
-        setLikedPosts(
-            posts.map( ( post ) =>
-                post.receivedLikes.some( ( like ) => like.userId === userId )
-            )
-        );
-    }, [posts, userId] );
+    const toggleComments = ( index: number ) => {
+        const newShowComments = [...showComments];
+        newShowComments[index] = !showComments[index];
+        setShowComments( newShowComments );
+    };
 
 
 
     return (
         <div className="max-w-2xl mx-auto">
             {posts.map( ( post: Post, index: number ) => (
-                <div key={post.id} className="border bg-white border-gray-300 mb-4 rounded overflow-hidden">
+                <div key={post.id} className="bg-white rounded-md overflow-hidden shadow-md mb-4">
+                    {/* Contenido del post */}
                     <div className="p-4">
                         <div className="flex items-center mb-2">
                             <div>
                                 <p className="text-sm font-semibold text-gray-600">{post.createdBy.username}</p>
-                                <p className="text-xs text-gray-500">
-                                    {format( new Date( post.createdAt ), 'dd/MM/yyyy h:mm a' )}
-                                </p>
+                                <p className="text-xs text-gray-500">{format( new Date( post.createdAt ), 'dd/MM/yyyy h:mm a' )}</p>
                             </div>
                         </div>
                         <h2 className="text-lg font-semibold mb-2">{post.title}</h2>
                         <p className="text-sm mb-4">{post.description}</p>
                     </div>
-                    <div className="p-4">
+
+                    {/* Likes y comentarios */}
+                    <div className="p-4 flex items-center justify-between">
                         <div className="flex items-center space-x-2">
                             <Button
                                 type="text"
@@ -74,32 +99,41 @@ const PostList: React.FC<PostListProps> = ( { posts } ) => {
                                 className={`text-xs text-gray-500 ${likedPosts[index] ? 'text-red-500' : ''}`}
                                 onClick={() => handleLike( index, post.id )}
                             >
-                                Like
+                            </Button>
+                            <Button icon={<CommentOutlined />} type="text" className=' text-xs text-gray-500' onClick={() => toggleComments( index )}>
+                                {showComments[index] ? '' : ''}
                             </Button>
                         </div>
-                        <p className="text-xs text-gray-500 mt-2">{likes[index]} likes</p>
+                        <p className="text-xs text-gray-500">{likes[index]} Me gusta</p>
+                    </div>
 
-                        {/* Comentarios usando Ant Design */}
-                        <div className="mt-4 max-h-36 overflow-hidden overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-300" style={{ overflowX: 'hidden' }}>
+                    {/* Comentarios */}
+                    {showComments[index] && (
+                        <div className="p-4 max-h-36 overflow-y-auto text-sm">
+
+                            {( !post.comments || post.comments.length === 0 ) ? 'No hay comentarios' : ''}
                             {post.comments.map( ( comment ) => (
-                                <div key={comment.id} className="mb-4 border-b pb-2">
-                                    <p className="text-lg font-bold">{comment.user.username}</p>
-                                    <p className="text-gray-700">{comment.comment}</p>
-                                    <span className="text-gray-500 block">{format( new Date( comment.createdAt ), 'dd/MM/yyyy h:mm a' )}</span>
+                                <div key={comment.id} className="mb-4">
+                                    <p className="text-sm text-gray-700 font-semibold">@{comment.user.username}</p>
+                                    <p className="text-gray-700 text-xs">{comment.comment}</p>
+                                    <span className="text-gray-500 block text-xs">{format( new Date( comment.createdAt ), 'dd/MM/yyyy h:mm a' )}</span>
                                 </div>
                             ) )}
                         </div>
+                    )}
 
-                        {/* Formulario para agregar comentarios usando Ant Design */}
-                        <div className="mt-2">
+                    {/* Formulario de comentarios */}
+                    <div className="p-4">
+                        <div className="flex items-center space-x-2">
                             <TextArea
-                                rows={3}
+                                className='w-full h-4 text-xs'
+                                rows={1}
                                 placeholder="Añadir un comentario..."
-                                value={newComment}
                                 onChange={( e ) => setNewComment( e.target.value )}
                             />
-                            <Button type="primary" onClick={() => handleAddComment( index, post.id )}>
-                                Publicar
+                            <Button icon={<SendOutlined />} className='text-xs' type="default" onClick={() => handleAddComment( index, post.id )}>
+
+
                             </Button>
                         </div>
                     </div>
